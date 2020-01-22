b891cfb882455dcce23a8df4874a4261eb717628
whiteFly <32365615+Sinf0r0s0@users.noreply.github.com>
2020-01-22 00:39:39 -0300

    Create stmmsg.py

---
+++ steamreedem/stmmsg.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""error messages"""
+mesg14 = 'The product code you\'ve entered is not valid. Please double check to see if you\'ve mistyped your key. I, L, and 1 can look alike, as can V and Y, and 0 and O.'
+msg15 = 'The product code you\'ve entered has already been activated by a different Steam account. This code cannot be used again. Please contact the retailer or online seller where the code was purchased for assistance.'
+mesg53 = 'There have been too many recent activation attempts from this account or Internet address. Please wait and try your product code again later.'
+mesg13 = 'Sorry, but this product is not available for purchase in this country. Your product key has not been redeemed.'
+mesg9 = 'This Steam account already owns the product(s) contained in this offer. To access them, visit your library in the Steam client.'
+mesg24 = 'The product code you\'ve entered requires ownership of another product before activation.\n\nIf you are trying to activate an expansion pack or downloadable content, please first activate the original game, then activate this additional content.'
+mesg36 = 'The product code you have entered requires that you first play this game on the PlayStation�3 system before it can be registered.\n\nPlease:\n\n- Start this game on your PlayStation�3 system\n\n- Link your Steam account to your PlayStation�3 Network account\n\n- Connect to Steam while playing this game on the PlayStation�3 system\n\n- Register this product code through Steam.'
+mesg50 = 'The code you have entered is from a Steam Gift Card or Steam Wallet Code. Browse here: https://store.steampowered.com/account/redeemwalletcode to redeem it.'
+mesg00 = 'An unexpected error has occurred.  Your product code has not been redeemed.  Please wait 30 minutes and try redeeming the code again.  If the problem persists, please contact <a href="https://help.steampowered.com/en/wizard/HelpWithCDKey">Steam Support</a> for further assistance.';
